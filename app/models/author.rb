1a2619fd32ce43b024225fef6d31686ba1fef964
pradeepshriffle <impradeepsharma20@gmail.com>
2023-08-17 18:32:19 +0530

    some modificationa extra in models

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,9 @@
 class Author < ApplicationRecord
     has_many :books, dependent: :destroy
-    validates :authorname, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "Only alphabetic characters allowed" }
+    validates :authorname, presence: true, uniqueness: true,format: { with: /\A[a-zA-Z]+\z/, message: "Only alphabetic characters allowed" }
+    validates :authoremail ,presence:true,uniqueness:true,format:{
+        with:/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,message: "plz enter valid email"}
+
+     
 end
   
