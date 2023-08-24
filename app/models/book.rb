105bd115c91d3fed9b29bcf0b3ce0c9665f177e7
pradeepshriffle <impradeepsharma20@gmail.com>
2023-08-24 10:53:31 +0530

    modified book and category

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,5 +5,14 @@ class Book < ApplicationRecord
 
   validates :booktitle, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z0-9\s]+\z/, message: "Only alphanumeric characters allowed" }
   validates :bookyear, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1500, less_than_or_equal_to: Date.current.year }
-  
+  after_create :book_success
+  after_destroy :book_destroy  
+
+  private 
+  def book_success
+    puts "Book added Sucessfully"
+  end 
+  def book_destroy
+    puts "Book destroy Sucessfully"
+  end  
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
     has_many :books
-    validates :categorytype, presence: true, format: { with: /\A[a-zA-Z\s]+\z/, message: "Only alphabetic characters allowed" }
+    validates :categorytype, presence: true,uniqueness: true, format: { with: /\A[a-zA-Z\s]+\z/, message: "Only alphabetic characters allowed" }
 end
